9bc04cac3e4830498b58652d32b4a7b5aaa486c2
Firdaus Mohamad <Ihm-Frd>
2024-03-18 02:18:57 +0700

    first commit

---
+++ _header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mohammad Ilham Firdaus - 312010313</title>
+
+	<!-- css bootstrap -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+	<style type="text/css">
+		body { font-family: "segoe ui", arial; }
+		div { padding: 5px 0; }
+		button { border: 0; padding: 5px; background-color: #39c; color: #fff; }
+		button:hover { background-color: #000; cursor: pointer; }
+		table.data th, table.data td { padding: 2px; } 
+		input[type=submit] { cursor: pointer; }
+	</style>
+</head>
+<body>
+	<?php include "konek_201651085.php"; ?>
